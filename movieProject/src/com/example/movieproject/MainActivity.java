c91ed4f8e2230004985e66000e49fe1fbd2f58ff
subashch6 <Subash@Subash-HP.columbus.rr.com>
2014-10-06 21:38:46 -0400

    commented out even more for debugging

--- movieProject/src/com/example/movieproject/MainActivity.java
+++ movieProject/src/com/example/movieproject/MainActivity.java
@@ -62,10 +62,10 @@ public class MainActivity extends Activity {
 		String date = Date.getText().toString();       */                   
 		//String newDate = date.charAt(0) + date.charAt(1) + "-" + date.charAt(3) + date.charAt(4);
 		//String url = "http://data.tmsapi.com/v1/movies/showings?startDate=2014-" + newDate + "&zip=" + zipcode + "&api_key=5xmxecwv3kw4z4ndb5d5kbg6";
-		String url =  "http://data.tmsapi.com/v1/movies/showings?startDate=2014-10-10&zip=43016&api_key=5xmxecwv3kw4z4ndb5d5kbg6";
+		//String url =  "http://data.tmsapi.com/v1/movies/showings?startDate=2014-10-10&zip=43016&api_key=5xmxecwv3kw4z4ndb5d5kbg6";
 		try
 		{
-			URL obj = new URL(url);
+			/*URL obj = new URL(url);
 			HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 			conn.setReadTimeout(5000);
 			conn.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
@@ -97,7 +97,8 @@ public class MainActivity extends Activity {
 		else
 		{
 		finalValue = "Hello";
-		}
+		}*/
+		String finalValue = "Hello";
 		Intent intent = new Intent(this, DisplayMessageActivity.class);
 		intent.putExtra(EXTRA_MESSAGE, finalValue);
 	    startActivity(intent);
